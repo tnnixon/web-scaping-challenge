2f74aec0aca905a405d7e02ad85ab5feef321108
Tiera Nixon <89324254+tnnixon@users.noreply.github.com>
2022-03-03 04:27:06 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,18 +4,33 @@ import scrape_mars
 
 app = Flask(__name__)
 
+# use the flask pymongo to setup the connection to the database
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data_db"
+mongo = PyMongo(app)
+
 @app.route("/")
 def index():
-    return "You reached the index"
+    # access information from the database
+    mars_data = mongo.db.marsData.find_one()
+    #print(mars_data)
+    return render_template("index.html", mars=mars_data)
+
 
 @app.route("/scrape")
 def scrape():
-    # return "You reached the scrape route"
+    # reference to a database collection (table)
+    marsTable = mongo.db.marsData
 
-    # Test to call scrape mars script
-    mars_data = scrape_mars.scrape_all
-    print(mars_data) # Print the dictionary that is returned from the scrape all script
-    return mars_data
+    # drop the table if it exists
+    mongo.db.marsData.drop()
+
+    #call scrape mars script
+    mars_data = scrape_mars.scrape_all()
+    
+    # take the dictionary and load it into mongoDB
+    marsTable.insert_one(mars_data)
+
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run()
